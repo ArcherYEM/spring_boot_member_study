266373379f7f1d20cb1ce12147d83c1874d7f3de
yangem <em.yang94@gmail.com>
2024-02-05 19:07:46 +0900

    dto, service 생성

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -1,12 +1,24 @@
 package com.codingrecipe.member.controller;
 
+import com.codingrecipe.member.dto.MemberDto;
+import com.codingrecipe.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // 롬복 에러로 인하여 하기 클래스 임의 추가
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm(){
@@ -14,11 +26,10 @@ public class MemberController {
     }
 
     @PostMapping("/member/save")
-    public String save(@RequestParam("memberEmail") String memberEmail
-        ,@RequestParam("memberPassword") String memberPassword
-        ,@RequestParam("memberName") String memberName){
+    public String save(@ModelAttribute MemberDto memberDto){
         System.out.println("MemberController.save");
-        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        System.out.println("memberDto = " + memberDto);
+
         return "index";
     }
 }
